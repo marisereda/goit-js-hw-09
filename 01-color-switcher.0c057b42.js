3710bdbe3045862645be3c6ccce2c7c4f763a931
marisereda <105078220+marisereda@users.noreply.github.com>
2022-09-26 08:16:25 +0000

    Deploying to gh-pages from @ marisereda/goit-js-hw-09@d9824344eba66595146d98c48a12ec9262d2c700 🚀

---
+++ 01-color-switcher.0c057b42.js
@@ -0,0 +1,2 @@
+!function(){var t=document.body,e=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]"),n=null;t.style.backgroundColor="#ffffff",d.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,n=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.0c057b42.js.map
